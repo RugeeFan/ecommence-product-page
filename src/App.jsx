a2e4b65d3bb2d2b56a27bdf4ec3eb65e6fafcac5
RugeFan <ruijiefan94@outlook.com>
2024-11-24 12:46:12 +1100

    Add new file

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ export default function App() {
     <div>
       <Navbar />
       <Sneaker />
+      <div className="attribution flex justify-center items-center">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+        Coded by <a href="#">Rugee</a>.
+      </div>
     </div>
   )
 }
